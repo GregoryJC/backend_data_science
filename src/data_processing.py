1a8a3f2d518ac64056702947b44699199f1cfbb9
k <am_straszenrand@posteo.de>
2022-05-04 11:26:25 +0200

    Add doc strings and README and example data

--- src/data_processing.py
+++ src/data_processing.py
@@ -6,7 +6,22 @@ import os
 import plotly.graph_objects as go
 
 # %% Get geoplot
-def get_geoplot(y, title_string):
+def get_geoplot(y : pd.core.frame.DataFrame, title_string : str):
+    """
+    A method to plot geo data mapped on a open-street-map layer
+
+    Parameter
+    ----------
+    y : pandas.core.frame.DataFrame
+        a pandas dataframe containing the geo data as 'lat' and 'lon' columns
+    title_string : str
+        the name of plot to be stored in folder 'visualizations/
+
+    Return
+    ----------
+    fig : plotly.graph_objs._figure.Figure
+        a tree-like data structure rendered by the plotly.js JavaScript library
+    """
 
     if "lat" in y and "lon" in y:
 
@@ -35,12 +50,44 @@ def get_geoplot(y, title_string):
 
         fig.write_html(f"../visualizations/{pd.Timestamp.now().strftime('%Y-%m-%d__%Hh%Mm')}__{title_string}.html")
 
+        return fig
+        
 # %% Get monthly parts of the data
-def get_month(y, month): 
+def get_month(y : pd.core.frame.DataFrame, month : str):
+    """
+    A method to filter the data by a given month
+
+    Parameter
+    ----------
+    y : pandas.core.frame.DataFrame
+        a pandas dataframe containing the month data as 'date' column
+    month : str
+        the name of month for wich the data should be filtered
+
+    Return
+    ----------
+    y : pandas.core.frame.DataFrame
+        a pandas dataframe containing filtered by the given month
+    """
 
     return y[y["date"].str.contains(month)]
 
 # %% Get min sum_raw_reads parts of the data
-def get_min_sum_raw_reads(y, threshold): 
+def get_min_sum_raw_reads(y : pd.core.frame.DataFrame, threshold : int): 
+    """
+    A method to filter the data by a given sum_raw_reads threshold
 
-    return y[y["sum_raw_reads"] > threshold]
+    Parameter
+    ----------
+    y : pandas.core.frame.DataFrame
+        a pandas dataframe containing the sum_raw_reads data as 'sum_raw_reads' column
+    threshold : int
+        the threshold that states the minimum amount of sum_raw_reads for wich the data should be filtered
+
+    Return
+    ----------
+    y : pandas.core.frame.DataFrame
+        a pandas dataframe containing filtered by the given threshold
+    """
+
+    return y[y["sum_raw_reads"] >= threshold]
